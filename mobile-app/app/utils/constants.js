27059845ac38b61666a0483d109d3f3c1bce643f
akash335 <pakashgoud355@gmail.com>
2026-04-26 07:18:21 +0530

    updated

--- mobile-app/app/utils/constants.js
+++ mobile-app/app/utils/constants.js
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native';
 
-const PROD_API_URL = 'https://your-backend-name.onrender.com';
-
+const PROD_API_URL = 'https://ckd-guardian-backend.onrender.com';
 const LOCAL_WEB_API_URL = 'http://127.0.0.1:8000';
 const LOCAL_ANDROID_API_URL = 'http://10.0.2.2:8000';
 const LOCAL_IOS_API_URL = 'http://127.0.0.1:8000';
